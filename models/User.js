74a32edcfeeabd5ed598310f06502819630b0acd
Muhammad Hammad Ali <mh6139251@gmail.com>
2026-05-27 21:26:45 +0500

    feat: Add isAdmin field to user schema for role management

--- models/User.js
+++ models/User.js
@@ -5,6 +5,11 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    isAdmin:{
+        type:Boolean,
+        default:false,
+        required:true
+    },
     email: {
         type: String,
         required: true,
